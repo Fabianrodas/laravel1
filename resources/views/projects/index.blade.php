06c01d11e64cdd88303ae0d3a3fafa63e5b32a90
Fabián Rodas <fabianrodas2005@gmail.com>
2025-05-18 13:46:21 -0500

    Primer commit con plantilla Laravel

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,15 @@
+<tbody>
+    @foreach($proyectos as $proyecto)
+    <tr>
+        <th scope="row">{{$proyecto->id}}</th>
+        <th>{{$proyecto->nombre}}</th>
+        <th>{{$proyecto->descripcion}}</th>  
+        <th>{{$proyecto->created_at}}</th>
+        <th>{{$proyecto->updated_at}}</th>
+        <td>
+        </td>
+        <td>
+        </td>
+    </tr>
+  @endforeach
+</tbody>
